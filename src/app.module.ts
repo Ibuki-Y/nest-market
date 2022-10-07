32c93e8eb926367895d6fad81ee58272afb3fa8f
Ibuki-Y <ibu.love.drum@gmail.com>
2022-10-08 02:38:00 +0900

    Add typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,33 @@
 import { Module } from '@nestjs/common';
 import { ItemsModule } from './items/items.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Item } from './items/entities/item.entity';
 
 @Module({
-  imports: [ItemsModule, TypeOrmModule.forRoot()],
+  imports: [
+    ItemsModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('POSTGRES_HOST'),
+        port: configService.get('POSTGRES_PORT'),
+        username: configService.get('POSTGRES_USER'),
+        password: configService.get('POSTGRES_PASSWORD'),
+        database: configService.get('POSTGRES_DB'),
+        autoLoadEntities: true,
+        entities: [Item],
+        migrations: ['src/migration/*.ts'],
+        // synchronize: true,
+        // cli: {
+        //   entitiesDir: 'src/entities',
+        //   migrationsDir: 'src/migrations',
+        // },
+      }),
+    }),
+  ],
   controllers: [],
   providers: [],
 })
