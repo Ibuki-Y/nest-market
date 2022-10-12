605da114455271e1d4e89d7bc5865293df41aff6
Ibuki-Y <ibu.love.drum@gmail.com>
2022-10-13 00:56:07 +0900

    Add auth files

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { ItemsModule } from './items/items.module';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     ItemsModule,
     PrismaModule,
+    AuthModule,
   ],
   controllers: [],
   providers: [],
