b1d25cbd4323e29c47c338acd1b9e93e78018445
Ibuki-Y <ibu.love.drum@gmail.com>
2022-10-13 17:11:55 +0900

    Add auth jwt login

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: {
+        expiresIn: 3600, // 1時間
+      },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { v4 as uuid } from 'uuid';
+import * as bcrypt from 'bcrypt';
 import { User } from '@prisma/client';
 import { PrismaService } from '../prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { CredentialsDto } from './dto/credentials.dto';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   async signup(createUserDto: CreateUserDto): Promise<User> {
+    const salt = await bcrypt.genSalt();
+    createUserDto.password = await bcrypt.hash(createUserDto.password, salt);
     const user = this.prisma.user.create({
       data: {
         id: uuid(),
@@ -17,5 +29,35 @@ export class AuthService {
     });
 
     return user;
+  }
+
+  async login(
+    credentialsDto: CredentialsDto,
+  ): Promise<{ accessToken: string }> {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        username: credentialsDto.username,
+      },
+    });
+    if (!user) {
+      throw new ForbiddenException('Username or password incorrected');
+    }
+
+    const isValid = await bcrypt.compare(
+      credentialsDto.password,
+      user.password,
+    );
+    if (!isValid) {
+      throw new ForbiddenException('Username or password incorrected');
+    }
+
+    try {
+      const payload = { id: user.id, username: user.username };
+      const accessToken = this.jwtService.sign(payload);
+
+      return { accessToken };
+    } catch (e) {
+      throw new UnauthorizedException('Check username or password');
+    }
   }
 }
